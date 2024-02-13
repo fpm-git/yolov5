a06a2a942404bf45cc3eb392eca2e907118e4ac0
zxc1139 <zhuocen93@gmail.com>
2024-02-13 10:28:57 -0800

    Able to track throughput and latency on wandb

--- utils/throughput_latency.py
+++ utils/throughput_latency.py
@@ -4,8 +4,6 @@ import time
 
 def measure_gpu_throughput(model, inputs):
     batch_size = inputs.size(0)
-    # inputs = inputs.to('cuda')
-    # model = model.to('cuda')
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
     start.record()
@@ -15,14 +13,21 @@ def measure_gpu_throughput(model, inputs):
     end.record()
     torch.cuda.synchronize()
     latency = start.elapsed_time(end)/1000
+    # print(latency)
+    # throughput = inputs.size(0) * batch_size / latency
     throughput = batch_size / latency
     return throughput
 
 
 def measure_latency_cpu_usage(model, test_inputs):
+    # process = psutil.Process()
+    # cpu_start = process.cpu_percent()
     start = time.time()
     predictions = model(test_inputs[0:1, :, :, :])
     # print(test_inputs.shape)
     end = time.time()
+    # print(start, end)
+    # cpu_end = process.cpu_percent()
     latency = 1000 * (end - start) #convert to ms
+    # cpu_usage = cpu_end - cpu_start
     return latency
